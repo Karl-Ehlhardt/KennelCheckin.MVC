1a4c54c51ed0f98ba33469125373b4825695bd1c
Karl Ehlhardt <ehlhardt_karl@gmail.com>
2021-01-09 23:40:36 -0500

    1/9EOD

--- KennelCheckin.MVC/Controllers/AccountController.cs
+++ KennelCheckin.MVC/Controllers/AccountController.cs
@@ -140,6 +140,8 @@ namespace KennelCheckin.MVC.Controllers
         [AllowAnonymous]
         public ActionResult Register()
         {
+            ViewBag.Name = new SelectList(new ApplicationDbContext().Roles
+                                    .ToList(), "Name", "Name");
             return View();
         }
 
@@ -163,9 +165,17 @@ namespace KennelCheckin.MVC.Controllers
                     // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
+                    await this.UserManager.AddToRoleAsync(user.Id, model.UserRoles);
 
+                    if (model.UserRoles == "Worker")
+                    {
+                        return RedirectToAction("Dashboard", "Kennel");
+                    }
                     return RedirectToAction("Index", "DogInfo");
                 }
+                ViewBag.Name = new SelectList(new ApplicationDbContext().Roles
+                                  .ToList(), "Name", "Name");
+
                 AddErrors(result);
             }
 
